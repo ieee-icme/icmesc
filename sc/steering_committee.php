4b1efea1adcd0f6c12a2ca5805555de3b9523498
Alvin(Xinyao) Sun <xinyao1@ualberta.ca>
2016-10-06 11:36:00 -0700

    1. Update committe page

--- sc/steering_committee.php
+++ sc/steering_committee.php
@@ -2,168 +2,173 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="description" content="_your description goes here_" />
-<meta name="keywords" content="_your,keywords,goes,here_" />
-<meta name="author" content="_your name goes here_ " />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="description" content="_your description goes here_"/>
+    <meta name="keywords" content="_your,keywords,goes,here_"/>
+    <meta name="author" content="_your name goes here_ "/>
 
-<link rel="stylesheet" type="text/css" href="default.css" media="screen" title="(screen)" />
-<link rel="stylesheet" type="text/css" href="print.css" media="print" />
+    <link rel="stylesheet" type="text/css" href="default.css" media="screen" title="(screen)"/>
+    <link rel="stylesheet" type="text/css" href="print.css" media="print"/>
 
-<title>Welcome to IEEE-ICME website</title>
+    <title>Welcome to IEEE-ICME website</title>
 </head>
 
 <body>
 <div id="toptabs">
-<p>Site Network: 
-<a class="activetoptab" href="#">Papers</a><span class="hide"> | </span>
-<a class="toptab" href="#">Projects</a><span class="hide"> | </span>
-<a class="toptab" href="#">Shop</a></p>
+    <p>Site Network:
+        <a class="activetoptab" href="#">Papers</a><span class="hide"> | </span>
+        <a class="toptab" href="#">Projects</a><span class="hide"> | </span>
+        <a class="toptab" href="#">Shop</a></p>
 </div>
 
 <div id="container">
-<div id="logo">
-<table width=100%><tr>
-        <td align=left><h1><a href="index.html">www.IEEE-ICME.org</a></h1></td>
-        <td align=right><img src="images/ieee_banner.png" /> </td>
-</tr></table>
-</div>
+    <div id="logo">
+        <table width=100%>
+            <tr>
+                <td align=left><h1><a href="index.html">www.IEEE-ICME.org</a></h1></td>
+                <td align=right><img src="images/ieee_banner.png"/></td>
+            </tr>
+        </table>
+    </div>
 
-<div id="navitabs">
-<h2 >Site menu:</h2>
-<?= nav_menu(); ?>
-</div>
-	
-<div id="desc">
-<?= desc(); ?>
-</div>
-    
-<div id="main">
-<h1>ICME Steering Committee</h1>
-<h2>2015-2017</h2>
-<h3>Chair:</h3>
-    <blockquote><h4>
-    	<font size="2">Haohong Wang</font>
-    </h4></blockquote>
-<br/>
-<!-- name, company, since, until -->
-	<h3>Finance Committee:</h3>
-	<blockquote><h4>
-			<font size="2">Chengcui Zhang (Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Anup Basu (Co-Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Zicheng Liu – ICME 2014 Program Chair, CAS MSATC Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Enrico Magli – ICME 2015 General Chair, SP MMSP Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Cha Zhang – ICME 2016 General Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Kenneth K. M. Lam – ICME 2017 General Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Wenjun Zeng – ICME 2018 General Chair</font>
-		</h4></blockquote>
-	<br/>
-	<h3>Paper Committee:</h3>
-	<blockquote><h4>
-			<font size="2">Tao Mei (Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Zhu Li (Co-Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Mei-Ling Shyu – CS TCMC Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Yonggang Wen – ComSoc MMTC Past Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Gene Cheung – SP MMSPTC</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Joern Ostermann – ICME 2017 General Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">C. C. Jay Kuo – ICME 2018 General Chair</font>
-		</h4></blockquote>
-	<br/>
-	<h3>Award Committee:</h3>
-	<blockquote><h4>
-			<font size="2">Min Chen (Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Periklis (Co-Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Qian Zhang – ICME 2017 Program Chair </font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Jin Li – ICME SC Past Chair</font>
-		</h4></blockquote>
-	<br/>
-	<h3>Program Committee:</h3>
-	<blockquote><h4>
-			<font size="2">Shipeng Li (Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Dong Xu (Co-Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Yap-Peng Tan – ICME 2015 Program Chair, ICME’10 General Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Phillip Chou – ICME 2016 Program Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Lina Karam – ICME 2017 Program Chair</font>
-		</h4></blockquote>
-	<br/>
-	<h3>Strategy Development Committee:</h3>
-	<blockquote><h4>
-			<font size="2">Irene Cheng (Chair) </font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Chia-Wen Lin (Co-Chair)</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Shiwen Mao – ComSoc MMTC Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Frossard Pascal – ICME 2002 General Chair</font>
-		</h4></blockquote>
-	<blockquote><h4>
-			<font size="2">Alexander C Loui – Past member of ICME Steering Committee</font>
-		</h4></blockquote>
-	<br/>
-	<h3>Others:</h3>
-	<blockquote><h4>
-			<font size="2">Alvin, Xinyao Sun - Webmaster </font>
-		</h4></blockquote>
-	<br/><br/>
-<br /><br />
-<h3><a href="past_members.php">Past Members</a></h3>
+    <div id="navitabs">
+        <h2>Site menu:</h2>
+        <?= nav_menu(); ?>
+    </div>
 
-</div>
+    <div id="desc">
+        <?= desc(); ?>
+    </div>
 
-<div id="sidebar">
-<h3>Navigation Links</h3>
-	<?= sidebar_menu(); ?>
+    <div id="main">
+        <h1>ICME Steering Committee</h1>
+        <h2>2015-2017</h2>
+        <h3>Chair:</h3>
+        <blockquote><h4>
+                <font size="2">Haohong Wang</font>
+            </h4></blockquote>
+        <br/>
+        <!-- name, company, since, until -->
+        <h3>Finance Committee:</h3>
+        <blockquote><h4>
+                <font size="2">Chengcui Zhang (Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Anup Basu (Co-Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Zicheng Liu – ICME 2014 Program Chair, CAS MSATC Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Enrico Magli – ICME 2015 General Chair, SP MMSP Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Ying Li– ICME 2016 Finance Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Kenneth K. M. Lam – ICME 2017 General Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Wenjun Zeng – ICME 2018 General Chair</font>
+            </h4></blockquote>
+        <br/>
+        <h3>Paper Committee:</h3>
+        <blockquote><h4>
+                <font size="2">Tao Mei (Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Zhu Li (Co-Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Mei-Ling Shyu – CS TCMC Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Yonggang Wen – ComSoc MMTC Past Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Gene Cheung – SP MMSPTC</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Joern Ostermann – ICME 2017 General Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">C. C. Jay Kuo – ICME 2018 General Chair</font>
+            </h4></blockquote>
+        <br/>
+        <h3>Award Committee:</h3>
+        <blockquote><h4>
+                <font size="2">Min Chen (Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Periklis (Co-Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Qian Zhang – ICME 2017 Program Chair </font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Jin Li – ICME SC Past Chair</font>
+            </h4></blockquote>
+        <br/>
+        <h3>Program Committee:</h3>
+        <blockquote><h4>
+                <font size="2">Shipeng Li (Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Dong Xu (Co-Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Yap-Peng Tan – ICME 2015 Program Chair, ICME’10 General Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Phillip Chou – ICME 2016 Program Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Lina Karam – ICME 2017 Program Chair</font>
+            </h4></blockquote>
+        <br/>
+        <h3>Strategy Development Committee:</h3>
+        <blockquote><h4>
+                <font size="2">Irene Cheng (Chair) </font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Chia-Wen Lin (Co-Chair)</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Shiwen Mao – ComSoc MMTC Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Frossard Pascal – ICME 2002 General Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Cha Zhang – ICME 2016 General Chair</font>
+            </h4></blockquote>
+        <blockquote><h4>
+                <font size="2">Alexander C Loui – Past member of ICME Steering Committee</font>
+            </h4></blockquote>
+        <br/>
+        <h3>Others:</h3>
+        <blockquote><h4>
+                <font size="2">Alvin, Xinyao Sun - Webmaster </font>
+            </h4></blockquote>
+        <br/><br/>
+        <br/><br/>
+        <h3><a href="past_members.php">Past Members</a></h3>
 
-<h3>IEEE-ICME</h3>
-<p><img class="photo" src="images/test.jpg" height="100" width="130" alt="" /></p>
+    </div>
 
-<? echo quote(); ?>
-</div>
+    <div id="sidebar">
+        <h3>Navigation Links</h3>
+        <?= sidebar_menu(); ?>
 
-<div id="footer">
-<? footer(); ?>
-</div>
+        <h3>IEEE-ICME</h3>
+        <p><img class="photo" src="images/test.jpg" height="100" width="130" alt=""/></p>
+
+        <? echo quote(); ?>
+    </div>
+
+    <div id="footer">
+        <? footer(); ?>
+    </div>
 
 </div>
 </body>
